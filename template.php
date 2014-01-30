e55c971c788d9533ca2e75196e24ca63448b5aaf
Ville Seppanen <ville.seppanen@gofore.com>
2014-01-30 15:56:51 +0200

    YH-168: Enable vertical menus to show full hierarchy

--- template.php
+++ template.php
@@ -87,3 +87,47 @@ function ytp_theme_preprocess_page(&$variables) {
 function ytp_theme_process_page(&$variables) {
   $variables['navbar_classes'] = implode(' ', $variables['navbar_classes_array']);
 }
+
+
+
+/**
+ * Overrides theme_menu_link().
+ * This fixes hierarchical vertical block menus when using Bootstrap theme.
+ */
+function ytp_theme_menu_link(&$variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it does not affect the navbar module.
+
+    /*if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {*/
+    if (($element['#original_link']['menu_name'] == 'management' && module_exists('navbar'))
+        || ((!empty($element['#original_link']['depth']))
+        && (isset($element['#bid']) && $element['#bid']['module'] == 'menu_block'))) {
+           $sub_menu = drupal_render($element['#below']);
+    }
+    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+      $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+      // Generate as standard dropdown.
+      $element['#title'] .= ' <span class="caret"></span>';
+      $element['#attributes']['class'][] = 'dropdown';
+      $element['#localized_options']['html'] = TRUE;
+
+      // Set dropdown trigger element to # to prevent inadvertant page loading when a submenu link is clicked.
+      $element['#localized_options']['attributes']['data-target'] = '#';
+      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+      $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+    }
+  }
+  // On primary navigation menu, class 'active' is not set on active menu item.
+  // @see https://drupal.org/node/1896674
+  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
