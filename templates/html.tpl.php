14893d0156e9330bc1cef5cbeef4fe5f60c123f0
Ilja <ilja@spam.fake>
2014-02-06 15:08:47 +0200

    Use only drupal styles

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -53,7 +53,6 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php include("/var/www/resources/templates/head-styles-drupal.html"); /* YTP common styles */ ?>
-  <?php include("/var/www/resources/templates/head-styles.html"); /* YTP common styles */ ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
