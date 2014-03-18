cd2fe7a1254a51d6dd5dfbda1310514036b14488
salum-ar <salum.abdul-rahman@gofore.com>
2014-03-18 15:55:23 +0200

    yh-178 fixed frontpage name localization in breadcrumbs

--- template.php
+++ template.php
@@ -175,4 +175,32 @@ function ytp_theme_menu_link(&$variables) {
   
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
+/**
+ * Overrides theme_breadcrumb().
+ *
+ * Print breadcrumbs as an ordered list.
+ */
+function ytp_theme_breadcrumb($variables) {
+  $output = '';
+  $breadcrumb = $variables['breadcrumb'];
+  if($breadcrumb){
+    if ($GLOBALS['language']->language="fi"){
+      $breadcrumb[0]="<a href=\"/fi\">Etusivu</a>";
+    }else if ($GLOBALS['language']->language="se"){
+      $breadcrumb[0]="<a href=\"/se\">Start</a>";
+    }
+  }
+  // Determine if we are to display the breadcrumb.
+  $bootstrap_breadcrumb = theme_get_setting('bootstrap_breadcrumb');
+  if (($bootstrap_breadcrumb == 1 || ($bootstrap_breadcrumb == 2 && arg(0) == 'admin')) && !empty($breadcrumb)) {
+    $output = theme('item_list', array(
+      'attributes' => array(
+        'class' => array('breadcrumb'),
+      ),
+      'items' => $breadcrumb,
+      'type' => 'ol',
+    ));
+  }
+  return $output;
+}
 
